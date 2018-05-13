0c6a946c66086c50513c40ecc77e40a29abb8cd1
Shravan Kumar Reddy Sandiri <shravanreddy.sandiri@gmail.com>
2018-05-13 16:45:41 -0400

    added user service

---
+++ src/main/webapp/jquery/services/user.service.client.js
@@ -0,0 +1,65 @@
+function UserServiceClient() {
+    this.createUser = createUser;
+    this.findAllUsers = findAllUsers;
+    this.findUserById = findUserById;
+    this.deleteUser = deleteUser;
+    this.updateUser = updateUser;
+    this.url = 'http://localhost:8080/api/user';
+    var self = this;
+    
+    
+    function createUser(user) {
+        return fetch(self.url, {
+            method: 'post',
+            body: JSON.stringify(user),
+            headers: {
+                'content-type': 'application/json'
+            }
+        });
+    }
+    
+    
+    
+    function findAllUsers() {
+        return fetch(self.url)
+        .then(function (response) {
+            return response.json();
+        });
+    
+    }
+    
+    
+    function findUserById(userId) {
+        return fetch(self.url + '/' + userId)
+            .then(function(response){
+                return response.json();
+            });
+    }
+    
+    function updateUser(userId,user) {  
+        return fetch(self.url + '/' + userId, {
+            method: 'put',
+            body: JSON.stringify(user),
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+        .then(function(response){
+            if(response.bodyUsed) {
+                return response.json();
+            } else {
+                return null;
+            }
+        });
+    	
+    	
+    	
+    }
+    function deleteUser(userId) { 
+    	
+    	   return fetch(self.url + '/' + userId, {
+               method: 'delete'
+           });
+    	
+    }
+}
