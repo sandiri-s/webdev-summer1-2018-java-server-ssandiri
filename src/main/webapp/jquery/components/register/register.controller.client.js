1f20857bcc7c6d621c6373884ccf9113ef5eeba2
Shravan Kumar Reddy Sandiri <shravanreddy.sandiri@gmail.com>
2018-05-13 22:51:42 -0400

    added register client controller

---
+++ src/main/webapp/jquery/components/register/register.controller.client.js
@@ -0,0 +1,36 @@
+(function () {
+	var $usernameFld, $passwordFld, $verifyPasswordFld;
+	var $registerBtn;
+	var userService = new UserServiceClient();
+	$(main);
+
+	function main() {
+		$('#signup-btn').click(register);
+
+	}
+	function register() { 
+
+		$usernameFld = $('#username').val();
+		$passwordFld = $('#password').val();
+		var user = new User();
+		user.setUsername($usernameFld);
+		user.setPassword($passwordFld);
+		userService
+		.register(user).then((res) => {
+			if(res.status==200)
+			{
+				alert("signup successful");
+			}
+
+			else if(res.status ==409)
+			{
+				alert("The username is already taken, please choose a new username");
+
+			}
+			else 
+			{
+				alert("signup unsuccessful, http error staus:" + "res.status");
+
+			}});
+	}
+})();
