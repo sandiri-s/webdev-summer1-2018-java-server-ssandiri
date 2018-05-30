8f73548f419f2b4527353d8bef43536a23525fed
Shravan Kumar Reddy Sandiri <shravanreddy.sandiri@gmail.com>
2018-05-30 03:52:07 -0400

    fixed another bug in widger repository

--- src/main/java/courseapp/repositories/WidgetRepository.java
+++ src/main/java/courseapp/repositories/WidgetRepository.java
@@ -2,6 +2,9 @@ package courseapp.repositories;
 
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -10,7 +13,9 @@ import courseapp.models.User;
 import courseapp.models.Widget;
 
 public interface WidgetRepository extends CrudRepository<Widget, Integer>{
-	
+
+	@Transactional
+	@Modifying
 	@Query("DELETE FROM Widget w WHERE w.lesson.id=:lessonId")
 	void deleteWidgetsByLessonId(
 		@Param("lessonId") int lessonId);
