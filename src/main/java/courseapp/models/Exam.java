8f7218d3c90c1e367219bbb6025483fb31faa52d
Shravan Kumar Reddy Sandiri <shravanreddy.sandiri@gmail.com>
2018-06-04 23:53:36 -0400

    added exam model

---
+++ src/main/java/courseapp/models/Exam.java
@@ -0,0 +1,35 @@
+package courseapp.models;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class Exam extends Widget {
+	private String title;
+	private String description;
+	@OneToMany(mappedBy="exam")
+	@JsonIgnore
+	private List<BaseExamQuestion> questions;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public List<BaseExamQuestion> getQuestions() {
+		return questions;
+	}
+	public void setQuestions(List<BaseExamQuestion> questions) {
+		this.questions = questions;
+	}
+}
