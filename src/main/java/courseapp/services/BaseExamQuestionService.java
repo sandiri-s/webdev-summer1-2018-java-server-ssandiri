0f2a6f6c51f29300177be70011447327f52b9b8a
Shravan Kumar Reddy Sandiri <shravanreddy.sandiri@gmail.com>
2018-06-07 17:00:12 -0400

    added delete controller for base question

--- src/main/java/courseapp/services/BaseExamQuestionService.java
+++ src/main/java/courseapp/services/BaseExamQuestionService.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -46,5 +46,9 @@ public class BaseExamQuestionService {
 		return null;
 	}
 	
+	@DeleteMapping("/api/question/{questionId}")
+	public void deleteExam(@PathVariable("questionId") int id) {
+		baseRepo.deleteById(id);
+	}
 	
 }
